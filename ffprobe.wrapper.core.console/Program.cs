e406da894109bdd3fcaa0789458257e2fd607322
mpcdigitize <plawrow@hotmail.com>
2020-06-30 18:18:19 -0400

    removed older project

--- ffprobe.wrapper.core.console/Program.cs
+++ ffprobe.wrapper.core.console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ffprobe.wrapper.core.Enums;
+using System;
 
 namespace ffprobe.wrapper.core.console
 {
@@ -7,6 +8,39 @@ namespace ffprobe.wrapper.core.console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            var reader = new MetadataReader(@"C:\ffmpeg\ffprobe.exe");
+            string localFile = @"C:\input\ABBA.mp3";
+            string streamPR3 = @"http://stream3.polskieradio.pl:8904/;stream";
+
+            var json = reader.DoWork(localFile, OutputFormat.Json);
+            var output = reader.ReadStream(localFile);
+
+
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine(json);
+
+
+
+            Console.WriteLine(output.NumberOfStreams);
+            Console.WriteLine(output.FormatName);
+            Console.WriteLine(output.DurationInSeconds);
+
+
+
+
+            foreach (var item in output.Streams)
+            {
+                Console.WriteLine(item.Index);
+                Console.WriteLine(item.Codec);
+                Console.WriteLine(item.CodecLongName);
+                Console.WriteLine(item.CodecType);
+                Console.WriteLine(item.Bitrate);
+
+
+            }
+
         }
     }
 }
